f002558da295bd2e2b693072a16ae2272f46b30e
rubimeryka <erykarubim13@gmail.com>
2023-07-10 13:46:08 -0300

    challenge palindromes recursive

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message("Hello World", "key")
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(1, 2)
+
+    assert encrypt_message("Hello World", 3) == "Hell_odl_lroW"
+    assert encrypt_message("Hello World", 6) == "Wro_llodl_eH"
+    assert encrypt_message("OpenAI", 5) == "AI_neO"
+    assert encrypt_message("", 0) == ""
+    assert encrypt_message("abc", 1) == "abc"
