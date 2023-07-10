1e1b9846a5a342edec0fce92d31dc828a1a2e963
rubimeryka <erykarubim13@gmail.com>
2023-07-10 09:08:53 -0300

    challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    counter = 0
+
+    for period in permanence_period:
+        if not all(isinstance(time, int) for time in period):
+            return None
+
+        if period[0] <= target_time <= period[1]:
+            counter += 1
+
+    return counter
